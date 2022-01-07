b31533cbf3a135cfe2c9e0dbe5e21d1d73406d58
Viola <violablanducci@gmail.com>
2022-01-07 10:47:54 +0800

    Test Something
    Reason for Commit:
      - dirvebase done???
    
    Bugs Fixed:
      - everything
    
    Bugs/Things to be Changed:
      - any problems
    
    Notes:
      - probubly wont work

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -1,12 +1,102 @@
 #include "Robot.h"
+#include "WMLCtre.h"
 
 using namespace frc;
-using namespace wml;
+// using namespace wml;
+
+// leftF m = ?
+// leftB m = ?
+// rightF m = ?
+// rightB m = ?
+
+// wml::TalonSrx {port}
+// wml::controllers::Joystick {port}
+
+// wml::TalonSrx leftF{99};
+// wml::TalonSrx leftB{99};
+// wml::TalonSrx rightF{99};
+// wml::TalonSrx rightB{99};
+
+wml::controllers::Joystick joy{0};
+
+class Drivetrain {
+ public:
+	Drivetrain(int topLeft = 0, int topRight = 1, int bottomLeft = 2, int bottomRight = 3) {
+		// Right motor init
+		_rightM1 = new wml::TalonSrx(topRight);
+		_rightM2 = new wml::TalonSrx(bottomRight);
+
+		// Left motor init
+		_leftM1 = new wml::TalonSrx(topLeft);
+		_leftM2 = new wml::TalonSrx(bottomLeft);
+
+		_leftM1->SetInverted(true);
+		_leftM2->SetInverted(true);
+	}
+
+	std::pair<double, double> get() {
+		return {_left, _right};
+	}
+
+	/**
+	 * Set left and right drivetrain
+	 */
+	void setTurret(double leftPower, double rightPower) {
+		_left = 0;
+		_right = 0;
+
+		// Left motors
+		_leftM1->Set(leftPower);
+		_leftM2->Set(leftPower);
+
+		// Right motors
+		_rightM1->Set(rightPower);
+		_rightM2->Set(rightPower);
+	}
+
+	void set(double joyX, double joyY, double joyZ) {
+		double left1 = 0, left2 = 0, right1 = 0, right2 = 0;
+
+		// Forwards back
+		if (joyY > 0.15) {
+			left1 += joyY;
+			left2 += joyY;
+			right1 += joyY;
+			right2 += joyY;
+		}
+
+		// Left right
+		if (joyX) {
+			left1 += -joyX;
+			left2 += joyX;
+			right1 += -joyX;
+			right2 += joyX;
+		}
+
+		// Rotation
+		if (joyZ) {
+			left1 += joyZ;
+			left2 += joyZ;
+			right1 += -joyZ;
+			right2 += -joyZ;
+
+		}
+		_leftM1->Set(left1);
+		_leftM2->Set(left2);
+		_rightM1->Set(right1);
+		_rightM2->Set(right2);	
+	}
+
+ private:
+	wml::TalonSrx *_rightM1, *_rightM2, *_leftM1, *_leftM2;
+	double _left = 0, _right = 0;
+};
+
+Drivetrain drive(99, 99, 99, 99);
 
 void Robot::RobotInit() {}
 
 void Robot::RobotPeriodic() {}
-
 
 void Robot::DisabledInit() {
 	InterruptAll(true);
@@ -17,8 +107,28 @@ void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
 // Manual Robot Logic
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopInit() {
+	// leftM.SetInverted(true);
+	// rightM.SetInverted(false);
+}
+void Robot::TeleopPeriodic() {
+	std::cout << "Robot code running" << std::endl;
+	// double leftPower = 0, rightPower = 0;
+	
+	// // Forwards and backwards
+	// if (fabs(joy.GetAxis(joy.kYAxis)) >= 0.15) {
+	// 	leftPower += joy.GetAxis(joy.kYAxis);
+	// 	rightPower += joy.GetAxis(joy.kYAxis);
+	// }
+	
+	// // Turning
+	// if (fabs(joy.GetAxis(joy.kZAxis)) >= 0.15) {
+	// 	leftPower += -joy.GetAxis(joy.kZAxis); 
+	// 	rightPower += joy.GetAxis(joy.kZAxis); 
+	// }
+
+	drive.set(fabs(joy.GetAxis(joy.kXAxis)), fabs(joy.GetAxis(joy.kYAxis)), fabs(joy.GetAxis(joy.kZAxis)));
+}
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
